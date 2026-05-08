35d41d2b5019accbb22619c4745637dcf1c6c3cf
Aidan Dempster <aidan.dempster@gmail.com>
2026-05-08 13:46:42 -0400

    Voice fix

--- mbot_hackathon/src/mbot_hackathon_pkg/nodes/text_to_speech_node.py
+++ mbot_hackathon/src/mbot_hackathon_pkg/nodes/text_to_speech_node.py
@@ -34,6 +34,7 @@ class MultilingualTTSNode(Node):
         for voice in voices:
             if language_code in voice.languages or language_code in voice.id:
                 if language_code == "en" and "merican" not in voice.name:
+                    self.get_logger().info(f"Skipping voice: {voice.name} ({language_code})")
                     continue
                 self.engine.setProperty('voice', voice.id)
                 self.get_logger().info(f"Voice changed to: {voice.name} ({language_code})")
